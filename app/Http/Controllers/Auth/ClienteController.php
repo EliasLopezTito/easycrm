ecfb73b24be7c96f71a4e384f5888481df5060f1
1UronaldU1 <ronlad_M_Q_2015@outlook.es>
2025-03-13 11:02:41 -0500

    se modifico para ver un error

--- app/Http/Controllers/Auth/ClienteController.php
+++ app/Http/Controllers/Auth/ClienteController.php
@@ -1005,6 +1005,10 @@ class ClienteController extends Controller
             }
         } catch (\Exception $e) {
             DB::rollBack();
+            return response()->json([
+                'Success' => false,
+                'Errors' => $e->getMessage()
+            ]);
         }
 
         return response()->json(['Success' => $status, 'Errors' => $validator->errors()]);
