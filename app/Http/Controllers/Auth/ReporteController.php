b635deb24c505fc9d2bf33c630ec4ce9dfb489f2
1UronaldU1 <ronlad_M_Q_2015@outlook.es>
2025-03-20 10:02:29 -0500

    verificación de reporte en con el super admin v5

--- app/Http/Controllers/Auth/ReporteController.php
+++ app/Http/Controllers/Auth/ReporteController.php
@@ -374,8 +374,9 @@ class ReporteController extends Controller
         }
 
         if ($userLogin->id == 1) {
-            $totalClientes->where("estado_id", 4)->count();
-            dd($arregloFilterEstados, $totalClientes);
+            $two = $this->obtenerTotalClientesCreadosPorFecha($userProfile, "2025-03-19", "2025-03-19", "created_at_last_contact");
+            $two->where("estado_id", 4)->count();
+            dd($arregloFilterEstados, $two);
         }
 
         if ($request->action_full == "true") {
