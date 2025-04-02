1cd1dbdb12de189d2d872aae4bb6307171dd93df
1UronaldU1 <ronlad_M_Q_2015@outlook.es>
2025-04-02 14:44:08 -0500

    se esta verificando las notificaciones para las asesoras v2

--- resources/views/auth/layout/app.blade.php
+++ resources/views/auth/layout/app.blade.php
@@ -199,7 +199,7 @@
 <script type="text/javascript" src="{{ asset('auth/plugins/sweetalert/sweetalert.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('auth/plugins/toastr/js/toastr.min.js') }}"></script>
 @if (Auth::user()->email == "useraul@gmail.com" || Auth::user()->email == "asesorprueba@gmail.com" || Auth::user()->email == "ronald@gmail.com")
-    <script type="text/javascript" src="{{ asset('auth/js/_Layout.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('auth/js/_LayoutV2.min.js') }}"></script>
 @else
     <script type="text/javascript" src="{{ asset('auth/js/_Layout.min.js') }}"></script>
 @endif
